dd4d7788b42f14d92d7ab3c9375201ef7ee24146
Alex Pacey <alex.pacey@mindlinksoft.com>
2026-09-15 15:25:54 +0100

    Added test for initialising rover with target direction and target position

---
+++ WakingSkeleton.Test/InitialiseTest.cs
@@ -0,0 +1,23 @@
+﻿namespace WakingSkeleton.Test;
+
+[TestFixture]
+public class InitialiseTest
+{
+    [Test]
+    public void initialiseRover()
+    {
+        Rover rover = new Rover();
+
+        int[] TargetPosition = new int[] { 2, 3 };
+        string TargetDirection = "North";
+
+        Assert.That(rover.position, Is.EqualTo(TargetPosition));
+        Assert.That(rover.direction, Is.EqualTo(TargetDirection));
+    }
+}
+
+public class Rover
+{
+    public int[] position { get; set; } = new int[] { 2, 3 };
+    public string direction { get; set; } = "North";
+}
